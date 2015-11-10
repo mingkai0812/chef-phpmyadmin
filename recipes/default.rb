ad32325d7473ad85da082531acb4e16cae86b9d0
Kai Lippert <mingkai0812@googlemail.com>
2015-11-10 10:07:23 +0100

    Removed module_mbstring, set loop to include other recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,10 +23,10 @@ require 'digest/sha1'
 # PHP Recipe includes we already know PHPMyAdmin needs
 if node['phpmyadmin']['stand_alone'] then
 	include_recipe 'php'
-	include_recipe 'php::module_mbstring'
-	include_recipe 'php::module_mcrypt'
-	include_recipe 'php::module_gd'
-	include_recipe 'php::module_mysql'
+
+	%w{php5-gd php5-mcrypt php5-mysql}.each do |pkg|
+		package pkg
+	end
 
 	directory node['phpmyadmin']['upload_dir'] do
 		owner 'root'
